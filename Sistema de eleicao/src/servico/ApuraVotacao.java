eb154a333bcf8c5168c9932a983b3a4a90aa3c0a
Cliente <Cliente@DESKTOP-PM9MF9K.mshome.net>
2025-05-28 13:28:37 -0300

    Terminado: 	-Classe "ApuraVotacao.java" Falta rever:

--- Sistema de eleicao/src/servico/ApuraVotacao.java
+++ Sistema de eleicao/src/servico/ApuraVotacao.java
@@ -9,34 +9,37 @@ public class ApuraVotacao {
 		String fileVotacao1 = "votacao1.txt";
 		String fileVotacao2 = "votacao2.txt";
 		
-		int c1 = 0;
-		int c2= 0;
 		
-		c1 = fleLinhaArquivo(fileVotacao1, c1);
-		Apuracao[] apurado1 = new Apuracao[c1];
+		int c1 = fleLinhaArquivo(fileVotacao1);
+		Apuracao[] v1 = new Apuracao[c1];
 		
-		c2 = fleLinhaArquivo(fileVotacao2, c2);
-		Apuracao[] apurado2 = new Apuracao[c2];
+		int c2 = fleLinhaArquivo(fileVotacao2);
+		Apuracao[] v2 = new Apuracao[c2];
 		
-		pleArquivo(apurado1, c1, fileVotacao1);
+		int[] votosApurados = new int[4];
 		
+		pleArquivo(v1, fileVotacao1);
+		pleArquivo(v2, fileVotacao2);
+		
+		votosApurados = fapuraVotos(v1,v2);
 		
 	}
 	
-	public int fleLinhaArquivo(String fileName, int contador)throws IOException {
+	public int fleLinhaArquivo(String fileName)throws IOException {
 		
+		int contador = 0;
 		BufferedReader ler = new BufferedReader(new FileReader(fileName));
 		
 		while(ler.readLine() != null) {
 			contador++;
 		}
-		System.out.println(contador);
 		
+		contador /=3;
 		return contador;
 	}
 	
 	
-	public void pleArquivo(Apuracao[] apurado, int tamanhoVetor, String fileName)throws IOException {
+	public void pleArquivo(Apuracao[] apurado, String fileName)throws IOException {
 		
 		BufferedReader ler = new BufferedReader(new FileReader(fileName));
 		
@@ -44,11 +47,60 @@ public class ApuraVotacao {
 		int secao = 0;
 		int codCandidato = 0;
 		
-		for (int i = 0; i < tamanhoVetor; i++) {
+		for (int i = 0; i < apurado.length; i++) {
 			numEleitor = Integer.parseInt(ler.readLine());
 			codCandidato = Integer.parseInt(ler.readLine());
 			secao = Integer.parseInt(ler.readLine());
+			
+			apurado[i] = new Apuracao(secao, codCandidato, numEleitor);
 		}
+		
+		
+	}
+	
+	public int[] fapuraVotos(Apuracao[] v1, Apuracao[] v2) {
+		int[] votosApurados = new int[4];
+		Apuracao[] v3 = new Apuracao[v1.length + v2.length];
+		
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		
+		while(i < v1.length && j < v2.length) {
+			if(v1[i].getCodCandidato() < v2[j].getCodCandidato() ) {
+				v3[k++] = v1[i++];
+			}else {
+				v3[k++] = v2[j++];
+			}
+		}
+		
+		while(i < v1.length) {
+			v3[k++] = v1[i++];
+		}
+		
+		while(j < v2.length) {
+			v3[k++] = v2[j++];
+		}
+		
+		votosApurados = fcontaVotos(v3);
+		
+		return votosApurados;
+	}
+	
+	
+	public int[] fcontaVotos(Apuracao[] v3) {
+		int[] votosApurados = new int [4];
+		int codCandidato = 1;
+		for(int i = 0; i < v3.length;i++) {
+			if(v3[i].getCodCandidato() == codCandidato) {
+				votosApurados[codCandidato-1]++;
+			}else {
+				votosApurados[codCandidato]++;
+				codCandidato++;
+			}
+		}
+		
+		return votosApurados;
 	}
 	
 }
